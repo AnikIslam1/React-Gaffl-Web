d9bf29fc86c70dcced05c298fcffd588ad15756f
Anik Islam <68261265+AnikIslam1@users.noreply.github.com>
2022-07-20 14:37:47 +0600

    Nav Update

--- gaffl/src/App.js
+++ gaffl/src/App.js
@@ -1,10 +1,19 @@
+import React from 'react';
 import './App.css';
+import Navbar from './components/navbar';
+
+import { BrowserRouter as Router} from 'react-router-dom';
+
 
 function App() {
   return (
-    <div className="App">
+    <Router>
+      <Navbar />
       
-    </div>
+        
+
+      
+    </Router>
   );
 }
 
---
+++ gaffl/src/components/navbar/index.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import {
+  Nav,
+  NavLink,
+  Bars,
+  NavMenu,
+  NavBtn,
+  NavBtnLink
+} from './nav';
+
+const Navbar = () => {
+  return (
+    <>
+      <Nav>
+        <NavLink to="/">
+          <img src={require('../../images/logo.png')} alt='logo' />
+        </NavLink>
+        <Bars />
+        <NavMenu>
+          <NavLink to='/Start-trip' activeStyle>
+            Start Trip
+          </NavLink>
+          <NavLink to='/Discover' activeStyle>
+            Discover
+          </NavLink>
+          <NavLink to='/Message' activeStyle>
+            Message
+          </NavLink>
+          <NavLink to='/Like' activeStyle>
+            Likes
+          </NavLink>
+          <NavLink to='/View' activeStyle>
+            Views
+          </NavLink>
+          {/* Second Nav */}
+          {/* <NavBtnLink to='/sign-in'>Sign In</NavBtnLink> */}
+        </NavMenu>
+        <NavBtn>
+          <NavBtnLink to='/get-unlimited'>Get Unlimited</NavBtnLink>
+        </NavBtn>
+      </Nav>
+    </>
+  );
+};
+
+export default Navbar;
